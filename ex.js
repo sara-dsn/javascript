f8f244b765abce72164379b70db4119e70acdc3d
sara-dsn <sabr.tdg@gmail.com>
2023-09-11 08:59:18 +0200

    first commit

---
+++ ex.js
@@ -0,0 +1,21 @@
+var nom = [a-Az-ZéèàêËîÏâ]-[a-Az-ZéèàêËîÏâ];
+document.write(nom)
+alert(nom);
+
+var prenom = [a-Az-ZéèàêËîÏâ]-[a-Az-ZéèàêËîÏâ];
+document.write(prenom)
+alert(prenom);
+
+var calcule=document.getElementById(calcule);
+console.log(typeof nombre);
+function calc()
+{
+    var n1 = parseInt(document.getElementById(n1).value);
+    var n2 =paresInt(document.getElementById(n2).value);
+    var signes=document.getElementById('signes').value;
+    if(signes === '+'){
+
+        var n3 =document.getElementById(n3).value= n1+n2;
+    }
+  
+}
