a93a85ea2caf79d0e0ccd363fefdb7bf37032248
sara-dsn <sabr.tdg@gmail.com>
2023-09-11 12:25:52 +0200

    ex4 complet js

--- ex4.js
+++ ex4.js
@@ -1,4 +1,12 @@
+var pre=window.prompt("Saisissiez votre prénom: ")
+var nom=window.prompt("Saisissiez votre nom: ")
+ window.alert("Votre prenom est: "+pre+"et votre nom de famille: "+nom)
+
 var n1=window.prompt("Saissisez un nombre:")
 var n2=window.prompt("Saissisez un autre nombre:")
 var resultat= n1*n2
 window.alert(resultat)
+
+var celsuis=window.prompt("Entrez la température en degré celsius: ")
+var fahren= (celsuis*9/5)+32
+window.alert(celsuis+"degré Celsuis correspont à "+fahren" degré Fahrenheit.")
