cd2a629dc54ce9c7be6411b15af46ffc7643c2e5
sara-dsn <sabr.tdg@gmail.com>
2023-09-11 12:00:11 +0200

    EX JS

--- ex.js
+++ ex.js
@@ -1,8 +1,10 @@
-var nom = [a-Az-ZéèàêËîÏâ]-[a-Az-ZéèàêËîÏâ];
+var nom=document.getElementById(nom);
+nom = [a-Az-ZéèàêËîÏâ]-[a-Az-ZéèàêËîÏâ];
 document.write(nom)
 alert(nom);
 
-var prenom = [a-Az-ZéèàêËîÏâ]-[a-Az-ZéèàêËîÏâ];
+var prenom=document.getElementById(prenom);
+ prenom = [a-Az-ZéèàêËîÏâ]-[a-Az-ZéèàêËîÏâ];
 document.write(prenom)
 alert(prenom);
 
@@ -11,13 +13,12 @@ console.log(typeof nombre);
 function calc()
 {
     var n1 = parseInt(document.getElementById(n1).value);
-    var n2 =paresInt(document.getElementById(n2).value);
+    var n2 =parseInt(document.getElementById(n2).value);
     var signes=document.getElementById('signes').value;
 
     if(signes === '+')
     {
-        document.getElementById(n3).value== n1+n2;
+        document.getElementById(n3).value==parseinit(n1)+parseinti(n2);
     }
   
 }
-var n3=parseinit(n1)+parseinti(n2);
---
+++ ex4.js
@@ -0,0 +1,4 @@
+var a=document.getElementById(ia);
+var b=100;
+var c =1.00;
+var b (Boolean)=vrai;
